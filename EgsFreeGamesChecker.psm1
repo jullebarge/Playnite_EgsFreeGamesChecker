2268c9a54fbef202532c7d7b65d2bb43f1b3c379
jullebarge <jullebarge@users.noreply.github.com>
2023-01-06 17:46:04 +0100

    Add files via upload
    Added a second option to add a tag instead of a category

--- EgsFreeGamesChecker.psm1
+++ EgsFreeGamesChecker.psm1
@@ -5,11 +5,17 @@ function GetMainMenuItems
     )
     
     $menuItem1 = New-Object Playnite.SDK.Plugins.ScriptMainMenuItem
-    $menuItem1.Description = [Playnite.SDK.ResourceProvider]::GetString("LOCEGS_Free_Games_Checker_MenuItemCheckEGSGivenGamesDescription")
+    $menuItem1.Description = [Playnite.SDK.ResourceProvider]::GetString("LOCEGS_Free_Games_Checker_MenuItemAddCatEGSGivenGamesDescription")
     $menuItem1.FunctionName = "Add-EgsGivenCat"
     $menuItem1.MenuSection = "@EGS Free Games checker"
 
-    return $menuItem1
+    $menuItem2 = New-Object Playnite.SDK.Plugins.ScriptMainMenuItem
+    $menuItem2.Description = [Playnite.SDK.ResourceProvider]::GetString("LOCEGS_Free_Games_Checker_MenuItemAddTagEGSGivenGamesDescription")
+    $menuItem2.FunctionName = "Add-EgsGivenTag"
+    $menuItem2.MenuSection = "@EGS Free Games checker"
+	
+    return $menuItem1, $menuItem2
+
 }
 
 function Add-EgsGivenCat
@@ -106,5 +112,102 @@ function Add-EgsGivenCat
 	
 	}	
 
-    $PlayniteApi.Dialogs.ShowMessage(([Playnite.SDK.ResourceProvider]::GetString("LOCEGS_Free_Games_Checker_ResultsMessage") -f  $countergivenGame, $catName, $CountercatAdded), $ExtensionName)
+    $PlayniteApi.Dialogs.ShowMessage(([Playnite.SDK.ResourceProvider]::GetString("LOCEGS_Free_Games_Checker_ResultsMessageCat") -f  $countergivenGame, $catName, $CountercatAdded), $ExtensionName)
+}
+
+function Add-EgsGivenTag
+{
+    param(
+        $scriptMainMenuItemActionArgs
+    )
+
+    $ExtensionName = "EGS Free Games checker"
+	$countergivenGame = 0
+    $CountertagAdded = 0
+    $tagName = [Playnite.SDK.ResourceProvider]::GetString("LOCEGS_Free_Games_Checker_CategoryName")
+    $tagEGS = $PlayniteApi.Database.Tags.Add($tagName)
+   
+    try {
+        $uri = "https://en.everybodywiki.com/List_of_free_Epic_Games_Store_games"
+        $req = Invoke-WebRequest $uri
+    } catch {
+        $errorMessage = $_.Exception.Message
+        $PlayniteApi.Dialogs.ShowErrorMessage(([Playnite.SDK.ResourceProvider]::GetString("LOCEGS_Free_Games_Checker_GivenListDownloadFailErrorMessage") -f $errorMessage), $ExtensionName)
+     return
+    }
+
+	$table = $req.ParsedHtml.getElementsByTagName('table')[0]
+	
+	$GivenEgsGames = @()
+	for ($x = 1; $x -lt $table.rows.length; $x++) {    
+            $GivenEgsGames += $table.rows[$x].cells[1].innerText.trim()
+	}
+
+	$__logger.Info("$ExtensionName - $($GivenEgsGames.count) games found in the EGS given list")
+ 
+    if ($GivenEgsGames.count -eq 0)
+    {
+        $PlayniteApi.Dialogs.ShowErrorMessage([Playnite.SDK.ResourceProvider]::GetString("LOCEGS_Free_Games_Checker_NoGivenGamesErrorMessage"), $ExtensionName)
+        return
+    }
+
+	foreach ($game in $PlayniteApi.Database.Games) {
+		#Ignore if game is not an EGS game
+		$__logger.Info($game.PluginId)
+		$gameLibraryPlugin = [Playnite.SDK.BuiltinExtensions]::GetExtensionFromId($game.PluginId)
+		if ($gameLibraryPlugin -ne 'EpicLibrary')
+        {
+            $__logger.Info("$ExtensionName - Game `"$($game.name)`" is not an EGS game")
+			continue
+        }
+		
+		#Verify if the EGS tag is already present
+		if ($null -ne $game.Tags)
+        {
+            $EGSTagPresent = $false
+            foreach ($tag in $game.Tags) {
+                if ($tag.Name -eq $tagName)
+                {
+                    $__logger.Info("$ExtensionName - Game `"$($game.name)`" already has `"$($tagName)`" tag")
+                    $EGSTagPresent = $true
+					$countergivenGame++
+                    break
+                }
+            }
+
+            if ($EGSTagPresent -eq $true)
+            {
+                continue
+            }
+        }
+	
+		$gameName = $game.name.ToLower() -replace '[^\p{L}\p{Nd}]', ''
+		foreach ($item in $GivenEgsGames){
+			$itemName = $item.ToLower() -replace '[^\p{L}\p{Nd}]', ''
+			
+			if ($itemName -eq $gameName)
+			{
+				$__logger.Info("$ExtensionName - Added free EGS tag to `"$($game.name)`".")
+				$CountertagAdded++
+				$countergivenGame++
+				# Add tag Id to game
+				if ($game.TagIds)
+				{
+					$game.TagIds += $tagEGS.Id
+					break
+				}
+				else
+				{
+					# Fix in case game has null TagIds
+					$game.TagIds = $tagEGS.Id
+					break
+				}			
+			}
+		}
+		# Update game in database
+		$PlayniteApi.Database.Games.Update($game)
+	
+	}	
+
+    $PlayniteApi.Dialogs.ShowMessage(([Playnite.SDK.ResourceProvider]::GetString("LOCEGS_Free_Games_Checker_ResultsMessageTag") -f  $countergivenGame, $tagName, $CountertagAdded), $ExtensionName)
 }
